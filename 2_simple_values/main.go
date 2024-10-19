dfd8a9f0de9260c4f3d2e55ea97c62ce6fc53893
joalvarez <alvarez.joaquinez@gmail.com>
2024-10-19 11:51:28 -0300

    chore(simple-values): add simple values

---
+++ 2_simple_values/main.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+  // simple value
+
+  // int
+  fmt.Println(1 + 1)
+
+  // string
+  fmt.Println("hello goo")
+
+  // bool
+  fmt.Println(true)
+  fmt.Println(false)
+
+  // float
+  fmt.Println(10.5)
+  fmt.Println(7.0 / 3.0)
+}
--- Makefile
+++ Makefile
@@ -2,3 +2,6 @@ GO = go run
 
 hello:
 	@$(GO) 1_hello_world/main.go
+
+values:
+	@$(GO) 2_simple_values/main.go
