bad13837068d334f798089f68669840e3c9d3e9b
joalvarez <alvarez.joaquinez@gmail.com>
2024-10-21 20:30:58 -0300

    chore(formatting): use the formatter to go :D

--- 2-simple_values/main.go
+++ 2-simple_values/main.go
@@ -3,19 +3,19 @@ package main
 import "fmt"
 
 func main() {
-  // simple value
+	// simple value
 
-  // int
-  fmt.Println(1 + 1)
+	// int
+	fmt.Println(1 + 1)
 
-  // string
-  fmt.Println("hello goo")
+	// string
+	fmt.Println("hello goo")
 
-  // bool
-  fmt.Println(true)
-  fmt.Println(false)
+	// bool
+	fmt.Println(true)
+	fmt.Println(false)
 
-  // float
-  fmt.Println(10.5)
-  fmt.Println(7.0 / 3.0)
+	// float
+	fmt.Println(10.5)
+	fmt.Println(7.0 / 3.0)
 }
--- 3-variables/main.go
+++ 3-variables/main.go
@@ -2,22 +2,21 @@ package main
 
 import "fmt"
 
-
 func main() {
-  // declaración total
-  var name string = "Neymar"
+	// declaración total
+	var name string = "Neymar"
 
-  // inferencia de tipos
-  var otherName = "Robertito"
+	// inferencia de tipos
+	var otherName = "Robertito"
 
-  // shorthand syntax
-  isAdult := true
+	// shorthand syntax
+	isAdult := true
 
-  var lastname string
-  lastname = "Alvarenga"
+	var lastname string
+	lastname = "Alvarenga"
 
-  fmt.Println(name)
-  fmt.Println(otherName)
-  fmt.Println(isAdult)
-  fmt.Println(lastname)
+	fmt.Println(name)
+	fmt.Println(otherName)
+	fmt.Println(isAdult)
+	fmt.Println(lastname)
 }
