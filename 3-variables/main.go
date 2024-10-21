ac4b65e351938938b3ee6511d44e68bbfbb04473
joalvarez <alvarez.joaquinez@gmail.com>
2024-10-21 20:17:41 -0300

    chore(variables): add variables examples

---
+++ 3-variables/main.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+
+func main() {
+  // declaración total
+  var name string = "Neymar"
+
+  // inferencia de tipos
+  var otherName = "Robertito"
+
+  // shorthand syntax
+  isAdult := true
+
+  var lastname string
+  lastname = "Alvarenga"
+
+  fmt.Println(name)
+  fmt.Println(otherName)
+  fmt.Println(isAdult)
+  fmt.Println(lastname)
+}
